6cf28787db2a2a72f59baace74b1e67e6da406b7
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-05-19 14:38:12 +0900

    클래스 실습

--- StudentList/Program.cs
+++ StudentList/Program.cs
@@ -8,8 +8,32 @@ namespace StudentList
 {
     class Program
     {
+        class Teacher
+        {
+            public string name;
+            public string major;
+        }
         static void Main(string[] args)
         {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() {name="함기훈", major = "뉴미디어콘텐트제작"});
+            list.Add(new Teacher() { name = "박성래", major = "수학" });
+            list.Add(new Teacher() { name = "민주리", major = "데이터베이스" });
+            list.Add(new Teacher() { name = "김보경", major = "가정" });
+            list.Add(new Teacher() { name = "장민주", major = "디자인" });
+
+            for(int i=list.Count-1;i>=0; i--)
+            {
+                if(list[i].major== "데이터베이스")
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            foreach (var item in list)
+            {
+                
+                Console.WriteLine(item.name+":"+ item.major);
+            }
         }
     }
 }
